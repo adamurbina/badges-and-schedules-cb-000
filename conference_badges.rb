34e072d8fad8aa5ff4719d887b2b55abdac51713
Adam Urbina <adamurbina@gmail.com>
2018-05-31 02:22:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
 
 def batch_badge_creator(array)
     newArray.new
     array.each do |name|
-        newArray.append("Hello, my name is #{name}.")
+        newArray.append(badge_maker(name))
     end
     newArray
 end
