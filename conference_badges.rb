5e6e8a9d3c31f5184e4bd11dc5b8c34db601fc31
Adam Urbina <adamurbina@gmail.com>
2018-05-31 02:33:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,5 +16,5 @@ def assign_rooms(array)
     array.each_with_index do |name, index|
         assignArray.push("Hello, #{name}! You'll be assigned to room #{index + 1}.")
     end
-
+    assignArray
 end
